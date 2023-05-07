54bcd079ae818180c4536034e0dad9da5b574cf9
Alexandra Carvalho <alexandra.carvalho334@gmail.com>
2023-05-07 13:48:56 +0100

    Bad solution

--- src/model_entities/Reservation.java
+++ src/model_entities/Reservation.java
@@ -6,15 +6,15 @@ import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
-	private Integer roomNumber; 
-	private Date checkin; 
+	private Integer roomNumber;
+	private Date checkin;
 	private Date checkout;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation() {
 	}
-	
+
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
 		this.roomNumber = roomNumber;
 		this.checkin = checkin;
@@ -36,29 +36,33 @@ public class Reservation {
 	public Date getCheckout() {
 		return checkout;
 	}
-	
+
 	public long duration() {
-		//calcula a diferença das duas datas em milisegundos 
+		// calcula a diferença das duas datas em milisegundos
 		long diff = checkout.getTime() - checkin.getTime();
-		//converte os milisegundos em Dias 
+		// converte os milisegundos em Dias
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkin, Date checkout) {
-		 this.checkin = checkin; 
-		 this.checkout = checkout;
+
+	public String updateDates(Date checkin, Date checkout) {
+
+		Date now = new Date();
+		if (checkin.before(now) || checkout.before(now)) {
+			return "Reservation dates for update must be future dates";
+		} 
+		if (checkout.before(checkin)) {
+			return "Check-out date must be after check-in date";
+		}
+
+		this.checkin = checkin;
+		this.checkout = checkout;
+		//se retorna nulo é porque nao deu nenhum erro
+		return null; 
 	}
-	
-	@Override 
+
+	@Override
 	public String toString() {
-		return "Room " 
-				+ roomNumber
-				+ ", checkin: " 
-				+ sdf.format(checkin) 
-				+ ", checkout: "
-				+ sdf.format(checkout)
-				+ ", " 
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", checkin: " + sdf.format(checkin) + ", checkout: " + sdf.format(checkout) + ", "
+				+ duration() + " nights";
 	}
 }
